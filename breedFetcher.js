93cb263320662aa8fb4d916a779d57c639533e72
Phat Luong <luongtanphat25@gmail.com>
2023-06-08 15:15:41 -0400

    refactor completed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,22 @@
 const request = require('request');
 
-const breed = process.argv[2];
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
-
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  const data = JSON.parse(body);
-  try {
-    console.log(`Description: ${data[0].description}`);
-  } catch (err) {
-    console.log('Not found');
-  }
+  request.get(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
   
-});
+    const data = JSON.parse(body);
+    try {
+      callback(null, data[0].description);
+    } catch (err) {
+      callback('Not found', null);
+    }
+    
+  });
+};
+
+module.exports = { fetchBreedDescription };
